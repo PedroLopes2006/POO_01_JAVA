1d147541fda9c44c12bf0422c9519eb59a743d7d
PedroLopes2006 <pedro_magnago_lopes@hotmail.com>
2026-02-09 20:28:47 -0300

    Add files via upload

---
+++ PedroA09022026/src/main/java/com/mycompany/pedroa09022026/Pessoa.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.pedroa09022026;
+
+/**
+ *
+ * @author alunolab13
+ */
+public class Pessoa {
+    String nome;
+    int idade;
+    
+    String Imprimir(){
+        return "O meu nome é " + nome + " e minha idade é " + idade + " anos.";
+    }
+}
---
+++ PedroA09022026/src/main/java/com/mycompany/pedroa09022026/Principal.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pedroa09022026;   
+/**
+ *
+ * @author Pedro
+ */
+public class Principal {
+
+    public static void main(String[] args) {
+        Pessoa p1 = new Pessoa();
+        p1.nome = "Pedro";
+        p1.idade = 19;
+ 
+        System.out.println(p1.Imprimir());
+    }
+}
